f0168b48f4280bca51937b3c4a0ff6fb1cb376b3
Handi Priyono <handimacbook@gmail.com>
2018-02-17 23:18:22 +0700

    add exercise 11

---
+++ exercise11-changeMe.js
@@ -0,0 +1,40 @@
+function changeMe(arr) {
+  var obj = {};
+  var nom = 1;
+
+  if (arr.length === 0) {
+    console.log(' "" ');
+  }
+
+  for (var i = 0; i < arr.length; i++) {
+    obj.firstName = arr[i][0];
+    obj.lastName = arr[i][1];
+    obj.gender = arr[i][2];
+
+    if (!arr[i][3] || arr[i][3] > 2018) {
+      obj.age = 'Invalid Birth Year';
+    } else {
+      obj.age = 2018 - arr[i][3];
+    }
+
+    console.log(nom + '. ' + obj.firstName + ' ' + obj.lastName + ':');
+    nom++;
+    console.log(obj);
+  }
+}
+
+// TEST CASES ONLY BRO!
+changeMe([
+  ['Christ', 'Evans', 'Male', 1982],
+  ['Robert', 'Downey', 'Male'],
+]); // 1. Christ Evans:
+// { firstName: 'Christ',
+//   lastName: 'Evans',
+//   gender: 'Male',
+//   age: 36 }
+// 2. Robert Downey:
+// { firstName: 'Robert',
+//   lastName: 'Downey',
+//   gender: 'Male',
+//   age: 'Invalid Birth Year' }
+changeMe([]); // ""
