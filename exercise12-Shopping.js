b5e2369431843e276df9ae8566a344665c0a3975
handipriyono <handimacbook@gmail.com>
2018-04-15 22:15:23 +0700

    update code

--- exercise12-Shopping.js
+++ exercise12-Shopping.js
@@ -1,10 +1,15 @@
 function shoppingTime(memberId, money) {
-  var belian = [];
-  var obj = {};
+  //HANDI
 
-  var sisa = money;
+  var daftar = [
+    ['Sepatu stacattu', 1500000],
+    ['Baju brand Zoro', 500000],
+    ['Baju brand H&N', 250000],
+    ['Sweater brand Uniklooh', 175000],
+    ['Casing Handphone', 50000],
+  ];
 
-  if (memberId === '' || memberId === undefined) {
+  if (!memberId) {
     return 'Mohon maaf, toko X hanya berlaku untuk member saja';
   }
 
@@ -12,45 +17,27 @@ function shoppingTime(memberId, money) {
     return 'Mohon maaf, uang tidak cukup';
   }
 
-  obj.memberId = memberId;
-  obj.money = money;
+  var daftarbeli = [];
+  var obj = {};
+  var sisa = money;
+  for (let i = 0; i < daftar.length; i++) {
+    obj.memberId = memberId;
+    obj.money = money;
+    obj.listPurchased = daftarbeli;
 
-  while (money >= 50000) {
-    if (money >= 1500000 && belian.includes('Sepatu Stacattu') === false) {
-      belian.push('Sepatu Stacattu');
-      money = money - 1500000;
-    } else if (money >= 500000 && belian.includes('Baju Zoro') === false) {
-      belian.push('Baju Zoro');
-      money = money - 500000;
-    } else if (money >= 250000 && belian.includes('Baju H&N') === false) {
-      belian.push('Baju H&N');
-      money = money - 250000;
-    } else if (money >= 175000 && belian.includes('Sweater Uniklooh') === false) {
-      belian.push('Sweater Uniklooh');
-      money = money - 175000;
-    } else if (
-      money >= 50000 &&
-      belian.includes('Casing Handphone') === false
-    ) {
-      belian.push('Casing Handphone');
-      money = money - 50000;
-    } else {
-      break;
+    if (sisa >= 50000 && sisa >= daftar[i][1]) {
+      sisa -= daftar[i][1];
+      obj.changeMoney = sisa;
+
+      daftarbeli.push(daftar[i][0]);
     }
   }
-
-  obj.listPurchased = belian;
-  obj.changeMoney = money;
 
   return obj;
 }
 
-
-
-// TEST CASES only Bro!
+// TEST CASES
 console.log(shoppingTime('1820RzKrnWn08', 2475000));
-
-
 //{ memberId: '1820RzKrnWn08',
 // money: 2475000,
 // listPurchased:
@@ -61,11 +48,11 @@ console.log(shoppingTime('1820RzKrnWn08', 2475000));
 //    'Casing Handphone' ],
 // changeMoney: 0 }
 console.log(shoppingTime('82Ku8Ma742', 170000));
-//{ memberId: '82Ku8Ma742',
-// money: 170000,
-// listPurchased:
-//  [ 'Casing Handphone' ],
-// changeMoney: 120000 }
+// //{ memberId: '82Ku8Ma742',
+// // money: 170000,
+// // listPurchased:
+// //  [ 'Casing Handphone' ],
+// // changeMoney: 120000 }
 console.log(shoppingTime('', 2475000)); //Mohon maaf, toko X hanya berlaku untuk member saja
 console.log(shoppingTime('234JdhweRxa53', 15000)); //Mohon maaf, uang tidak cukup
 console.log(shoppingTime()); ////Mohon maaf, toko X hanya berlaku untuk member saja
